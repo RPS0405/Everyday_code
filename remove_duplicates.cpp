e00c7bd2bd00ab538c0fbfdbff0decb9bd99af05
Rudra Pratap Singh <rudrapratapsingh.an@gmail.com>
2024-10-25 21:45:01 +0530

    remove duplicates from string

---
+++ remove_duplicates.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include<string>
+
+
+
+void RemoveDuplicates(string str, string ans, int i, int map[]){
+    
+    if(i==str.size()){
+        
+        cout<<ans<<endl;
+        
+        return;
+    }
+    char ch= str[i];
+    int mapIndex= (int)(ch-'a');
+    
+    if(map[mapIndex]){
+        RemoveDuplicates(str, ans, i+1, map);
+    }
+    else{
+        map[mapIndex]=true;
+        RemoveDuplicates(str, ans+str[i], i+1, map);
+    }
+}
+
+
+int main (){
+    
+    string str= "aabbbcddd";
+    string ans= " ";
+    int map[26]= {false};
+    
+    RemoveDuplicates(str, ans, 0, map);
+    
+    
+    return 0;
+}
